cd4e91bebf11fd99cc599150efe74db9589b1efd
Wharton <BW069340@cerner.net>
2021-08-04 10:27:24 -0500

    Cleaning up this directory

--- Get-TermStatus.ps1
+++ Get-TermStatus.ps1
@@ -19,52 +19,53 @@ function Get-TermStatus
 		[Parameter(Mandatory = $false,
 				   ValueFromPipeline = $True,
 				   ValueFromPipelineByPropertyName = $true,
-                   ParameterSetName = "Paste",
+				   ParameterSetName = "Paste",
 				   Position = 0)]
 		[switch]$Paste,
-        [parameter(mandatory = $True,
-                  ValueFromPipeline = $True,
-                  ValueFromPipelineByPropertyName = $True,
-                  ParameterSetName = "FirstLast")]
-        [string]$First,
-        [parameter(mandatory = $True,
-                  ValueFromPipeline = $True,
-                  ValueFromPipelineByPropertyName = $True,
-                  ParameterSetName = "FirstLast")]
-        [string]$Last
+		[parameter(mandatory = $True,
+				   ValueFromPipeline = $True,
+				   ValueFromPipelineByPropertyName = $True,
+			 	   ParameterSetName = "FirstLast",
+				   Position = 0)]
+		[string]$First,
+		[parameter(mandatory = $True,
+				   ValueFromPipeline = $True,
+				   ValueFromPipelineByPropertyName = $True,
+				   ParameterSetName = "FirstLast",
+				   Position = 1)]
+		[string]$Last
 	)
 	
 	Begin
 	{
-        if($Paste.IsPresent)
-        {
-		    $users = Get-Clipboard
-        }
-        $gridview_object = @()
+		if ($Paste.IsPresent)
+		{
+			$users = Get-Clipboard
+		}
+		$gridview_object = @()
 	}
 	Process
 	{
-        if($Paste.IsPresent)
-        {
-		    foreach ($user in $users)
-		    {
-                if($user -ne "")
-                {
-			        $user_split = $user.split()
-			        $first = $user_split[0]
-			        $last = $user_split[1]
-			        $gridview_object += (get-firstlast $first $last)
-                }
-		    }
-        }
-        else
-        {
-            $gridview_object += Get-FirstLast $First $Last
-        }
+		if ($Paste.IsPresent)
+		{
+			foreach ($user in $users)
+			{
+				if ($user -ne "")
+				{
+					$user_split = $user.split()
+					$first = $user_split[0]
+					$last = $user_split[1]
+					$gridview_object += (get-firstlast $first $last)
+				}
+			}
+		}
+		else
+		{
+			$gridview_object += Get-FirstLast $First $Last
+		}
 	}
 	End
 	{
-        $gridview_object | Out-GridView
+		$gridview_object | ogv
 	}
 }
-
